69690d75f47137f2567333a4303bba1273c959e2
Nikolaos <foton263@gmail.com>
2015-03-14 13:06:56 +0200

    assignment code in progress...

---
+++ PA_1.R
@@ -0,0 +1,46 @@
+# Data analysis for reproducible Research 
+# default options
+Sys.setlocale("LC_TIME", "English")
+opts_chunk$set(echo=TRUE, results='asis')
+# read the dataset
+wd<-getwd()
+fn<-paste(wd,"/activity.csv",sep = "")
+dset <- read.csv(fn, header = TRUE,stringsAsFactors=FALSE)
+# exam the structure
+str(dset)
+head(dset,n = 1000)
+tail(dset,n = 1000)
+# transforming the variables
+
+# convert dates to proper type
+dset$date<-as.Date(dset$date)
+# format intervals, extract hours and minutes
+ival<-formatC(dset$interval,digits = 0,width = 4,flag = "0000")
+dset$hour<-substr(ival,start = 1,stop = 2)
+dset$mins<-substr(ival,start = 3,stop = 4)
+
+# What is mean total number of steps taken per day?
+# For this part of the assignment, you can ignore the missing values in the dataset.
+
+# 1. Make a histogram of the total number of steps taken each day
+##### Compute the column and plot the histogram
+stepspday <- aggregate(steps ~ date, data=dset, FUN="sum", na.exclude=TRUE)
+hist(stepspday$steps,breaks = 12,
+     xlab = "No of steps / day",
+     ylab = "% fraction of days",
+     main = "Total number of steps",
+     col = "orange",border = "purple"
+     )
+# 2. Calculate and report the mean and median total number of steps taken per day
+mean(stepspday$steps, na.rm=TRUE)
+median(stepspday$steps, na.rm=TRUE)
+
+# What is the average daily activity pattern?
+
+# 1. Make a time series plot (i.e. type = "l") of the 5-minute interval (x-axis) and the average number of steps taken, averaged across all days (y-axis)
+meanspinterval <- aggregate(steps ~ sInterval, data=dset, FUN="mean", na.exclude=T)
+
+# 2. Which 5-minute interval, on average across all the days in the dataset, contains the maximum number of steps?
+
+
+
